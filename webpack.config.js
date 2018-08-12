4958fb5dd15f2337ade195cb7215288abe763b01
Chris Kinch <christopher_kinch@dennis.co.uk>
2018-08-12 20:41:11 +0100

    DD-349 Renamed files for consistency

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require("path");
 
 module.exports = {
-    entry: require.resolve("./js/stickyElement.js"),
+    entry: require.resolve("./js/stickyElements.js"),
     mode: 'production',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "stickyElement.js",
+        filename: "stickyElements.min.js",
         libraryTarget: 'var',
         library: 'StickyElements'
     },
     watchOptions: {
-        poll: 1000 // Check for changes every second
+        poll: 200 // Check for changes every second
     },
     devtool: "source-map",
     module: {
